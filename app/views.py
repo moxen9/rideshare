feb5f96e5ed3e9cb7f86efd2ee63176f67566318
Chance Zibolski <chance.zibolski@gmail.com>
2013-03-07 21:40:56 -0800

    Create match view and bootstrap map template
    Add jquery for simplicity

--- app/views.py
+++ app/views.py
@@ -6,3 +6,8 @@ mod = Blueprint('app', __name__)
 @mod.route('/', methods=['GET'])
 def get():
     return render_template('index.html', user=g.user)
+
+
+@mod.route('/matching/', methods=['get', 'post'])
+def matching():
+    return render_template('map.html', user=g.user)
